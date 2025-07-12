5c79e539e9d3d6005af8dabd2777672e70975786
Stevn <165967402+Stevengt1@users.noreply.github.com>
2025-07-12 03:13:50 -0600

    Add Cart
    Se agrego la pagina para el carrito
    Se creo el archivo CSS
    Se creo un archivo JS

---
+++ vetJs/vetScriptsCart.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function() {
+    eventListeners();
+});
+
+function eventListeners() {
+    console.log("Aqui detecta el DOMContentLoaded");
+};
+
+// Crear una para mostrar los objetos del carrito en el Popover
